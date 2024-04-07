12e2a60ddac9765e9d353f48d31206ddd3d5807e
maximus-lazer <mlazerb@gmail.com>
2024-04-06 23:17:30 -0400

    Tested map with custom struct with dummy data

--- wire_analyze.cpp
+++ wire_analyze.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
-// create a list of machines participating in ARP
-struct ARP_info {
-	std::string MAC_address;	// their associated MAC address
-	std::string IP_address;	//  their associated IP address
-	// u_int16_t 
-	// constructor
-	ARP_info(std::string MAC_address, std::string IP_address) : MAC_address(MAC_address), IP_address(IP_address) {}
-};
+#include <map>
+#include "wire_handlers.h"
 
-//struct packet_info {
-	// start date and time of packet capture
-	// duration of packet capture in seconds with microsecond resolution
-	// total number of packets
-	// 1 list for unique senders
-	// total number of packets associated with unique senders
-	// 1 list for  unique recipients
-	// total number of packets associated with uniqeu recipients
 
-	// list of machines participating in ARP
-	// their associated MAC addresses
-	// their associated IP addresses
-
-	// 1 list of UDP unique source ports seen
-	// 1 list of UDP unique destination ports seen
-
-	// average packet size
-	// minimum packet size
-	// maximum packet size 
-
-//	packet_info(int 
-//};
 
 int main() {
+
+	std::map<int, struct prog_output> packetInfo;
+
+	struct prog_output a;
+	a.packet_number = 1;
+	struct prog_output b;
+	b.packet_number = 100;
+
+	packetInfo[8] = a;
+	packetInfo[42] = b;
+
+	// Get an iterator pointing to the first element in the
+    // map
+    std::map<int, struct prog_output>::iterator it = packetInfo.begin();
+ 
+    // Iterate through the map and print the elements
+    while (it != packetInfo.end()) {
+        std::cout << "Key: " << it->first
+             << ", Value: " << (it->second).packet_number << std::endl;
+        ++it;
+    }
+
+	/*
 	std::string line;
 	while(std::getline(std::cin, line)) {
 		std::cout << "line" << std::endl;
 		std::cout << line << std::endl;
 	}
+	*/
 	return 0;
 }
